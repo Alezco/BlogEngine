1c6097d7dee552600dda94681b0ffd8324d7968c
Benjamin Morali <benjaminmorali4@hotmail.fr>
2017-07-10 11:56:22 +0200

    Resolve merge conflict

--- src/main/java/controllers/ArticleController.java
+++ src/main/java/controllers/ArticleController.java
@@ -1,14 +1,23 @@
 package controllers;
 
+import models.Article;
+import services.Services;
+
 import javax.faces.bean.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.ArrayList;
 
 @RequestScoped
 @Named("article")
 public class ArticleController {
-    public void listArticles() {
+    @Inject
+    private Services services;
+
+    public ArrayList<Article> listArticles() {
         System.out.println("================");
         System.out.println("LOL");
         System.out.println("================");
+        return services.getList(Article.class);
     }
 }
