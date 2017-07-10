6778d2746c9c34aeed457e77eeee2959b0bc6517
Benjamin Morali <benjaminmorali4@hotmail.fr>
2017-07-10 09:59:18 +0200

    Add generic services

---
+++ src/main/java/services/Services.java
@@ -0,0 +1,33 @@
+package services;
+
+import dao.DAO;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.ArrayList;
+
+@ApplicationScoped
+public class Services {
+    private @Inject
+    DAO dao;
+
+    public <T> ArrayList<T> getList(Class tClass) {
+        return dao.getList(tClass);
+    }
+
+    public <T> Object getById(Class<T> tClass, int id) {
+        return dao.getById(tClass, id);
+    }
+
+    public <T> void create(T object) {
+        dao.create(object);
+    }
+
+    public <T> void update(T object) {
+        dao.update(object);
+    }
+
+    public <T> void delete(T object) {
+        dao.delete(object);
+    }
+}
