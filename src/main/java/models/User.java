1334969f4c302e675b4f24bbffb20658d821d138
Moussa Oufkir <moussa.oufkir@epita.fr>
2017-07-10 09:48:29 +0200

    [config] source dir and first user model

---
+++ src/main/java/models/User.java
@@ -0,0 +1,35 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String username;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private List<Blog> blogs;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
