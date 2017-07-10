0779f88bb559ad8b79d4d8820373a04c6a60d09e
Benjamin Morali <benjaminmorali4@hotmail.fr>
2017-07-10 16:41:49 +0200

    Update article files location

--- src/main/java/controllers/ArticleController.java
+++ src/main/java/controllers/ArticleController.java
@@ -3,16 +3,19 @@ package controllers;
 import models.Article;
 import services.Services;
 
-import javax.faces.bean.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-@RequestScoped
+@SessionScoped
 @Named("articleController")
-public class ArticleController {
+public class ArticleController implements Serializable {
     @Inject
     private Services services;
+
+    private Article currentArticle;
 
     public ArrayList<Article> listArticles() {
         return services.getList(Article.class);
@@ -21,5 +24,9 @@ public class ArticleController {
     public void addArticle(String title, String content) {
         Article article = new Article(title, content);
         services.create(article);
+    }
+
+    public void setCurrentArticle(Article article) {
+        this.currentArticle = article;
     }
 }
