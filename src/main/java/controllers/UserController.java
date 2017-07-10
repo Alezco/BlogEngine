e7dc5857265f1fc42de3f4adb991ab48e4a8779e
lorris saint-genez <lorris.saint-genez@epita.fr>
2017-07-10 11:21:46 +0200

    [Ctrl] Add user controller

---
+++ src/main/java/controllers/UserController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOError;
+
+/**
+ * Created by Lorris on 10/07/2017.
+ */
+
+@RequestScoped
+@Named("user")
+public class UserController {
+
+    public void signUp(String email, String password) {
+        System.out.println(email);
+        System.out.println(password);
+    }
+}
